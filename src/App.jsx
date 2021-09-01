ea365745ae9fa90a3141956a145b898d571bea1d
Ivan Falcon <ivan.falcon@whiz.pe>
2021-09-01 16:30:17 -0500

    add sass and small structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { NavBar } from './components';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" width="200" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
