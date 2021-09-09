a702dbd451485f4b95a9ca5e3be360df2d8bce55
Ivan Falcon <ivan.falcon@whiz.pe>
2021-09-09 14:31:08 -0500

    add counter component with button

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -4,21 +4,27 @@ import './ItemCount.scss';
 // import IconMinus from '../../img/icons/minus.svg';
 // import IconPlus from '../../img/icons/minus.svg';
 
-const ItemCount = ({ stock, initial, onAddButton }) => {
+const ItemCount = ({ stock, initial }) => {
   const [quantity, setQuantity] = useState(initial);
+  const inStock = !!(stock && stock > 0);
+  console.log('inStock', inStock);
+
+  console.log('demo');
 
   const decrease = () => {
     if (quantity > initial) {
       setQuantity((initialValue) => initialValue - 1);
-      onAddButton(quantity - 1);
     }
   };
 
   const increase = () => {
     if (quantity < stock) {
       setQuantity((initialValue) => initialValue + 1);
-      onAddButton(quantity + 1);
     }
+  };
+
+  const onAdd = () => {
+    alert(`Cantidad agregada: ${quantity}`);
   };
 
   return (
@@ -42,6 +48,9 @@ const ItemCount = ({ stock, initial, onAddButton }) => {
           onClick={increase}
         ></button>
       </div>
+      <button className="btn" onClick={onAdd} disabled={!inStock}>
+        AGREGAR A CARRITO
+      </button>
     </>
   );
 };
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,20 +1,15 @@
 import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = () => {
-  const onAdd = (quantity) => {
-    console.log(
-      'Soy el padre de counter y resivo la cantidad desde mi hijo counter:',
-      quantity
-    );
-  };
-
   return (
     <div className="container">
       <p>Contenedor de los cards</p>
 
       <div className="cards-box">
         <a className="card" href="#!">
-          <picture>{/* <img src="" alt="" /> */}</picture>
+          <picture>
+            <img src="img/no-photo.png" alt="No hay imagen" />
+          </picture>
 
           <div className="summary">
             <h2 className="name">Lorem Ipsum Lorem Ipsum</h2>
@@ -24,9 +19,7 @@ const ItemListContainer = () => {
             </p>
           </div>
 
-          <ItemCount stock={10} initial={1} onAddButton={onAdd} />
-
-          <button className="btn">AGREGAR A CARRITO</button>
+          <ItemCount stock={5} initial={1} />
         </a>
       </div>
     </div>
