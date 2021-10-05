a85e6a7542e524760370eb6207eb3e3b5d691e29
Ivan Falcon <ivan.falcon@whiz.pe>
2021-10-05 17:57:58 -0500

    change style product cart and add data base for firebase

---
+++ src/mocks/base.js
@@ -0,0 +1,37 @@
+/*
+{
+  "available": true,
+  "brand": "Funko POP!",
+  "categoryId": 2,
+  "description": "",
+  "discount_percentage": 0,
+  "id": 14,
+  "images": [
+    {
+      "src": "",
+      "alt": ""
+    },
+    {
+      "src": "",
+      "alt": ""
+    }
+  ],
+  "original_price": 80,
+  "price": 50,
+  "published": true,
+  "rating": 0,
+  "sku": "",
+  "slug": "",
+  "stock": 10,
+  "title": ""
+},
+*/
+
+// PRICES:
+// 50/80
+// 100/120
+// 150/180
+
+// Animatio: 0-4
+// Serie: 5-9
+// Película: 10-14
